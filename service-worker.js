518edb8e06271d56282e9b4b3a60379e05c89761
rainybug <kimbunchico@gmail.com>
2025-06-15 04:20:12 +0900

    handler added

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,7 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => self.clients.claim());
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+
+// 이 부분이 반드시 필요해요!
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
