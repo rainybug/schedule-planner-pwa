08447c03005dc500743e4f470e5eeb8e49d2c492
rainybug <kimbunchico@gmail.com>
2025-06-15 04:32:18 +0900

    screenshot added

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,14 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
+// public/service-worker.js
 
-// 이 부분이 반드시 필요해요!
+// 필수: 등록 후 즉시 활성화
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', () => {
+  self.clients.claim();
+});
+
+// 필수: fetch 핸들러 없으면 설치 불가!
 self.addEventListener('fetch', (event) => {
   event.respondWith(fetch(event.request));
 });
